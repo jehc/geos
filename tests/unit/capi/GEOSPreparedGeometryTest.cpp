jehc/geos

commit b051beb6b28103867fa887d8021fbf11518f6745

Add missing prepared geometry predicates to C API (#436)
git-svn-id: http://svn.osgeo.org/geos/trunk@3322 5242fede-7e19-0410-aef8-94bd7d2200fb

src/geom/prep/AbstractPreparedPolygonContains.cpp
@@ -92,12 +92,12 @@ AbstractPreparedPolygonContains::findAndClassifyIntersections(const geom::Geomet
 	intDetector = new noding::SegmentIntersectionDetector(li);
 
 	intDetector->setFindAllIntersectionTypes( true);
-
 	prepPoly->getIntersectionFinder()->intersects(&lineSegStr, intDetector);
-		
+  /*
 	hasSegmentIntersection = intDetector->hasIntersection();
 	hasProperIntersection = intDetector->hasProperIntersection();
 	hasNonProperIntersection = intDetector->hasNonProperIntersection();
+  */
 
 	delete intDetector;
 	delete li;
@@ -149,7 +149,7 @@ AbstractPreparedPolygonContains::eval( const geom::Geometry * geom)
 	bool properIntersectionImpliesNotContained = isProperIntersectionImpliesNotContainedSituation( geom);
 
 	// find all intersection types which exist
-	findAndClassifyIntersections( geom);
+  findAndClassifyIntersections( geom);
 
 	if ( properIntersectionImpliesNotContained && hasProperIntersection )
 		return false;

capi/geos_c.cpp
@@ -1012,15 +1012,51 @@ GEOSPreparedContainsProperly(const geos::geom::prep::PreparedGeometry *pg1, cons
 }
 
 char
+GEOSPreparedCoveredBy(const geos::geom::prep::PreparedGeometry *pg1, const Geometry *g2)
+{
+    return GEOSPreparedCoveredBy_r( handle, pg1, g2 );
+}
+
+char
 GEOSPreparedCovers(const geos::geom::prep::PreparedGeometry *pg1, const Geometry *g2)
 {
     return GEOSPreparedCovers_r( handle, pg1, g2 );
 }
 
 char
+GEOSPreparedCrosses(const geos::geom::prep::PreparedGeometry *pg1, const Geometry *g2)
+{
+    return GEOSPreparedCrosses_r( handle, pg1, g2 );
+}
+
+char
+GEOSPreparedDisjoint(const geos::geom::prep::PreparedGeometry *pg1, const Geometry *g2)
+{
+    return GEOSPreparedDisjoint_r( handle, pg1, g2 );
+}
+
+char
 GEOSPreparedIntersects(const geos::geom::prep::PreparedGeometry *pg1, const Geometry *g2)
 {
     return GEOSPreparedIntersects_r( handle, pg1, g2 );
+}
+
+char
+GEOSPreparedOverlaps(const geos::geom::prep::PreparedGeometry *pg1, const Geometry *g2)
+{
+    return GEOSPreparedOverlaps_r( handle, pg1, g2 );
+}
+
+char
+GEOSPreparedTouches(const geos::geom::prep::PreparedGeometry *pg1, const Geometry *g2)
+{
+    return GEOSPreparedTouches_r( handle, pg1, g2 );
+}
+
+char
+GEOSPreparedWithin(const geos::geom::prep::PreparedGeometry *pg1, const Geometry *g2)
+{
+    return GEOSPreparedWithin_r( handle, pg1, g2 );
 }
 
 STRtree *

capi/geos_c.h.in
@@ -772,8 +772,14 @@ extern void GEOS_DLL GEOSPreparedGeom_destroy(const GEOSPreparedGeometry* g);
 
 extern char GEOS_DLL GEOSPreparedContains(const GEOSPreparedGeometry* pg1, const GEOSGeometry* g2);
 extern char GEOS_DLL GEOSPreparedContainsProperly(const GEOSPreparedGeometry* pg1, const GEOSGeometry* g2);
+extern char GEOS_DLL GEOSPreparedCoveredBy(const GEOSPreparedGeometry* pg1, const GEOSGeometry* g2);
 extern char GEOS_DLL GEOSPreparedCovers(const GEOSPreparedGeometry* pg1, const GEOSGeometry* g2);
+extern char GEOS_DLL GEOSPreparedCrosses(const GEOSPreparedGeometry* pg1, const GEOSGeometry* g2);
+extern char GEOS_DLL GEOSPreparedDisjoint(const GEOSPreparedGeometry* pg1, const GEOSGeometry* g2);
 extern char GEOS_DLL GEOSPreparedIntersects(const GEOSPreparedGeometry* pg1, const GEOSGeometry* g2);
+extern char GEOS_DLL GEOSPreparedOverlaps(const GEOSPreparedGeometry* pg1, const GEOSGeometry* g2);
+extern char GEOS_DLL GEOSPreparedTouches(const GEOSPreparedGeometry* pg1, const GEOSGeometry* g2);
+extern char GEOS_DLL GEOSPreparedWithin(const GEOSPreparedGeometry* pg1, const GEOSGeometry* g2);
 
 /* 
  * GEOSGeometry ownership is retained by caller
@@ -791,12 +797,30 @@ extern char GEOS_DLL GEOSPreparedContains_r(GEOSContextHandle_t handle,
 extern char GEOS_DLL GEOSPreparedContainsProperly_r(GEOSContextHandle_t handle,
                                          const GEOSPreparedGeometry* pg1,
                                          const GEOSGeometry* g2);
+extern char GEOS_DLL GEOSPreparedCoveredBy_r(GEOSContextHandle_t handle,
+                                          const GEOSPreparedGeometry* pg1,
+                                          const GEOSGeometry* g2);
 extern char GEOS_DLL GEOSPreparedCovers_r(GEOSContextHandle_t handle,
+                                          const GEOSPreparedGeometry* pg1,
+                                          const GEOSGeometry* g2);
+extern char GEOS_DLL GEOSPreparedCrosses_r(GEOSContextHandle_t handle,
+                                          const GEOSPreparedGeometry* pg1,
+                                          const GEOSGeometry* g2);
+extern char GEOS_DLL GEOSPreparedDisjoint_r(GEOSContextHandle_t handle,
                                           const GEOSPreparedGeometry* pg1,
                                           const GEOSGeometry* g2);
 extern char GEOS_DLL GEOSPreparedIntersects_r(GEOSContextHandle_t handle,
                                               const GEOSPreparedGeometry* pg1,
                                               const GEOSGeometry* g2);
+extern char GEOS_DLL GEOSPreparedOverlaps_r(GEOSContextHandle_t handle,
+                                          const GEOSPreparedGeometry* pg1,
+                                          const GEOSGeometry* g2);
+extern char GEOS_DLL GEOSPreparedTouches_r(GEOSContextHandle_t handle,
+                                          const GEOSPreparedGeometry* pg1,
+                                          const GEOSGeometry* g2);
+extern char GEOS_DLL GEOSPreparedWithin_r(GEOSContextHandle_t handle,
+                                          const GEOSPreparedGeometry* pg1,
+                                          const GEOSGeometry* g2);
 
 /************************************************************************
  *

capi/geos_ts_c.cpp
@@ -5096,6 +5096,42 @@ GEOSPreparedContainsProperly_r(GEOSContextHandle_t extHandle,
 }
 
 char
+GEOSPreparedCoveredBy_r(GEOSContextHandle_t extHandle,
+        const geos::geom::prep::PreparedGeometry *pg, const Geometry *g)
+{
+    assert(0 != pg);
+    assert(0 != g);
+
+    if ( 0 == extHandle )
+    {
+        return 2;
+    }
+
+    GEOSContextHandleInternal_t *handle = 0;
+    handle = reinterpret_cast<GEOSContextHandleInternal_t*>(extHandle);
+    if ( 0 == handle->initialized )
+    {
+        return 2;
+    }
+
+    try 
+    {
+        bool result = pg->coveredBy(g);
+        return result;
+    }
+    catch (const std::exception &e)
+    {
+        handle->ERROR_MESSAGE("%s", e.what());
+    }
+    catch (...)
+    {
+        handle->ERROR_MESSAGE("Unknown exception thrown");
+    }
+    
+    return 2;
+}
+
+char
 GEOSPreparedCovers_r(GEOSContextHandle_t extHandle,
         const geos::geom::prep::PreparedGeometry *pg, const Geometry *g)
 {
@@ -5132,6 +5168,78 @@ GEOSPreparedCovers_r(GEOSContextHandle_t extHandle,
 }
 
 char
+GEOSPreparedCrosses_r(GEOSContextHandle_t extHandle,
+        const geos::geom::prep::PreparedGeometry *pg, const Geometry *g)
+{
+    assert(0 != pg);
+    assert(0 != g);
+
+    if ( 0 == extHandle )
+    {
+        return 2;
+    }
+
+    GEOSContextHandleInternal_t *handle = 0;
+    handle = reinterpret_cast<GEOSContextHandleInternal_t*>(extHandle);
+    if ( 0 == handle->initialized )
+    {
+        return 2;
+    }
+
+    try 
+    {
+        bool result = pg->crosses(g);
+        return result;
+    }
+    catch (const std::exception &e)
+    {
+        handle->ERROR_MESSAGE("%s", e.what());
+    }
+    catch (...)
+    {
+        handle->ERROR_MESSAGE("Unknown exception thrown");
+    }
+    
+    return 2;
+}
+
+char
+GEOSPreparedDisjoint_r(GEOSContextHandle_t extHandle,
+        const geos::geom::prep::PreparedGeometry *pg, const Geometry *g)
+{
+    assert(0 != pg);
+    assert(0 != g);
+
+    if ( 0 == extHandle )
+    {
+        return 2;
+    }
+
+    GEOSContextHandleInternal_t *handle = 0;
+    handle = reinterpret_cast<GEOSContextHandleInternal_t*>(extHandle);
+    if ( 0 == handle->initialized )
+    {
+        return 2;
+    }
+
+    try 
+    {
+        bool result = pg->disjoint(g);
+        return result;
+    }
+    catch (const std::exception &e)
+    {
+        handle->ERROR_MESSAGE("%s", e.what());
+    }
+    catch (...)
+    {
+        handle->ERROR_MESSAGE("Unknown exception thrown");
+    }
+    
+    return 2;
+}
+
+char
 GEOSPreparedIntersects_r(GEOSContextHandle_t extHandle,
         const geos::geom::prep::PreparedGeometry *pg, const Geometry *g)
 {
@@ -5153,6 +5261,114 @@ GEOSPreparedIntersects_r(GEOSContextHandle_t extHandle,
     try 
     {
         bool result = pg->intersects(g);
+        return result;
+    }
+    catch (const std::exception &e)
+    {
+        handle->ERROR_MESSAGE("%s", e.what());
+    }
+    catch (...)
+    {
+        handle->ERROR_MESSAGE("Unknown exception thrown");
+    }
+    
+    return 2;
+}
+
+char
+GEOSPreparedOverlaps_r(GEOSContextHandle_t extHandle,
+        const geos::geom::prep::PreparedGeometry *pg, const Geometry *g)
+{
+    assert(0 != pg);
+    assert(0 != g);
+
+    if ( 0 == extHandle )
+    {
+        return 2;
+    }
+
+    GEOSContextHandleInternal_t *handle = 0;
+    handle = reinterpret_cast<GEOSContextHandleInternal_t*>(extHandle);
+    if ( 0 == handle->initialized )
+    {
+        return 2;
+    }
+
+    try 
+    {
+        bool result = pg->overlaps(g);
+        return result;
+    }
+    catch (const std::exception &e)
+    {
+        handle->ERROR_MESSAGE("%s", e.what());
+    }
+    catch (...)
+    {
+        handle->ERROR_MESSAGE("Unknown exception thrown");
+    }
+    
+    return 2;
+}
+
+char
+GEOSPreparedTouches_r(GEOSContextHandle_t extHandle,
+        const geos::geom::prep::PreparedGeometry *pg, const Geometry *g)
+{
+    assert(0 != pg);
+    assert(0 != g);
+
+    if ( 0 == extHandle )
+    {
+        return 2;
+    }
+
+    GEOSContextHandleInternal_t *handle = 0;
+    handle = reinterpret_cast<GEOSContextHandleInternal_t*>(extHandle);
+    if ( 0 == handle->initialized )
+    {
+        return 2;
+    }
+
+    try 
+    {
+        bool result = pg->touches(g);
+        return result;
+    }
+    catch (const std::exception &e)
+    {
+        handle->ERROR_MESSAGE("%s", e.what());
+    }
+    catch (...)
+    {
+        handle->ERROR_MESSAGE("Unknown exception thrown");
+    }
+    
+    return 2;
+}
+
+char
+GEOSPreparedWithin_r(GEOSContextHandle_t extHandle,
+        const geos::geom::prep::PreparedGeometry *pg, const Geometry *g)
+{
+    assert(0 != pg);
+    assert(0 != g);
+
+    if ( 0 == extHandle )
+    {
+        return 2;
+    }
+
+    GEOSContextHandleInternal_t *handle = 0;
+    handle = reinterpret_cast<GEOSContextHandleInternal_t*>(extHandle);
+    if ( 0 == handle->initialized )
+    {
+        return 2;
+    }
+
+    try 
+    {
+        bool result = pg->within(g);
         return result;
     }
     catch (const std::exception &e)

src/geom/prep/PreparedPolygon.cpp
@@ -93,11 +93,11 @@ contains( const geom::Geometry * g) const
     {
         geom::Geometry const& geom = getGeometry();
         geom::Polygon const& poly = dynamic_cast<geom::Polygon const&>(geom);
-        
+       
         return operation::predicate::RectangleContains::contains(poly, *g);
     }
 
-	return PreparedPolygonContains::contains( this, g);
+	  return PreparedPolygonContains::contains(this, g);
 }
 
 bool 

commit b6afad54455901637a100faacc36b6570aa6a8d8

Fix regression accidentally introduced by commit r3322 (Sean's). Add automated test for it (was found trough PostGIS regression test)
git-svn-id: http://svn.osgeo.org/geos/trunk@3325 5242fede-7e19-0410-aef8-94bd7d2200fb

src/geom/prep/AbstractPreparedPolygonContains.cpp
@@ -93,11 +93,10 @@ AbstractPreparedPolygonContains::findAndClassifyIntersections(const geom::Geomet
 
 	intDetector->setFindAllIntersectionTypes( true);
 	prepPoly->getIntersectionFinder()->intersects(&lineSegStr, intDetector);
-  /*
+
 	hasSegmentIntersection = intDetector->hasIntersection();
 	hasProperIntersection = intDetector->hasProperIntersection();
 	hasNonProperIntersection = intDetector->hasNonProperIntersection();
-  */
 
 	delete intDetector;
 	delete li;

tests/unit/capi/GEOSPreparedGeometryTest.cpp
@@ -139,6 +139,38 @@ namespace tut
 
     }
 
+    // Test PreparedCovers
+    template<>
+    template<>
+    void object::test<5>()
+    {
+    geom1_ = GEOSGeomFromWKT("POLYGON((0 0, 0 10, 10 11, 10 0, 0 0))");
+    geom2_ = GEOSGeomFromWKT("POLYGON((0 0, 2 0, 2 2, 0 2, 0 0))");
+    prepGeom1_ = GEOSPrepare(geom1_);
+
+    ensure(0 != prepGeom1_);
+
+    int ret = GEOSPreparedCovers(prepGeom1_, geom2_);
+    ensure_equals(ret, 1);
+
+    }
+
+    // Test PreparedContains
+    template<>
+    template<>
+    void object::test<6>()
+    {
+    geom1_ = GEOSGeomFromWKT("POLYGON((0 0, 0 10, 10 11, 10 0, 0 0))");
+    geom2_ = GEOSGeomFromWKT("POLYGON((0 0, 2 0, 2 2, 0 2, 0 0))");
+    prepGeom1_ = GEOSPrepare(geom1_);
+
+    ensure(0 != prepGeom1_);
+
+    int ret = GEOSPreparedContains(prepGeom1_, geom2_);
+    ensure_equals(ret, 1);
+
+    }
+
     // TODO: add lots of more tests
     
 } // namespace tut